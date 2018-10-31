86496ba984bfc4155ea3f4e96f8cb745331502ca
Kirill Paromonov <kirillblur@gmail.com>
2018-10-31 13:09:13 +0200

    Added sliders

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,47 @@
+/* var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("gray").getElementsByClassName("quote");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "flex";
+} */
 
 
 $(document).ready(()=>{
+  $('.qoute-container').data('slideIndex',0);
+  $('.qoute-container').children('.btn').on('click',event=>{
+    let curIndex=$(event.currentTarget).parent().data('slideIndex');
+
+    var $quotes=$(event.currentTarget).parent().find('.quote-content');
+    $($quotes[curIndex]).fadeOut(300,()=>{
+      if($(event.currentTarget).hasClass('left')){
+        curIndex-=1;
+      }
+      else if($(event.currentTarget).hasClass('right')){
+        curIndex+=1;
+      }
+
+      if(curIndex==-1){curIndex=$quotes.length-1;}
+      else if(curIndex==$quotes.length){curIndex=0;}
+      $($quotes[curIndex]).css('display','flex')
+      .hide()
+      .fadeIn(300);
+      $(event.currentTarget).parent().data('slideIndex',curIndex);
+    });
+    //alert(slideIndex);
+
+  });
+
   $(document).on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
